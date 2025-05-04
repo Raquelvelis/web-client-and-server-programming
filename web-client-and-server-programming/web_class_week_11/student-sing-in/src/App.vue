57f0014723e04628e695735b42f44aea08b6de81
r.qui <r.qui@gmail.com>
2025-05-04 16:08:20 -0500

    Assignment 11 Completed

---
+++ web-client-and-server-programming/web_class_week_11/student-sing-in/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+
+import NewStudentForm from './components/NewStudentForm.vue'
+import StudentTable from './components/StudentTable.vue'
+import StudentMessage from './components/StudentMessage.vue'
+
+</script>
+
+<template>
+
+
+  <NewStudentForm>  </NewStudentForm>
+  <StudentTable />
+  <StudentMessage />
+</template>
+
+<style scoped>
+
+</style>
